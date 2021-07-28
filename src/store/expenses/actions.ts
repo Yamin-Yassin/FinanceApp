824c83f5618c07d7bc112b7b22d56fe53fb1b90c
Yamin Yassin <x210621@bcpcorp.net>
2021-07-28 16:58:47 +0100

    added actions

--- src/store/expenses/actions.ts
+++ src/store/expenses/actions.ts
@@ -1,5 +1,9 @@
 import {createAction} from 'typesafe-actions';
 
-export const addTransaction = createAction('add', action => {
+export const addBalance = createAction('ADD', action => {
+  return (value: string) => action(value);
+});
+
+export const removeBalance = createAction('REMOVE', action => {
   return (value: string) => action(value);
 });
