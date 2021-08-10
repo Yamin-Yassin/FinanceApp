bda7dfd5a333108456160e9e87b2b35fe3940955
Yamin Yassin <x210621@bcpcorp.net>
2021-08-10 12:04:08 +0100

    added account reducer

--- src/store/expenses/reducers.ts
+++ src/store/expenses/reducers.ts
@@ -1,9 +1,9 @@
-import {actionUsersType} from './types';
+import {actionAccountType} from './types';
 
 const initState = {
   id: '9f7b5b90-8f9d-4ace-b05e-6022b4b75bd3',
-  name: 'Yamin',
-  initialValue: 100,
+  name: null,
+  initialValue: 0,
   transactions: [
     {
       createDate: '2021-02-22T17:54:05.8534634+00:00',
@@ -38,37 +38,35 @@ const initState = {
 
 const accountReducer = (state = initState, action: any) => {
   switch (action.type) {
-    case actionUsersType.Request:
-      console.log('ACCOUNT REDUCER', actionUsersType.Request);
+    case actionAccountType.Request:
+      console.log('ACCOUNT REDUCER', actionAccountType.Request);
+
       return {
         ...state,
         loading: true,
       };
-    case actionUsersType.Success:
-      const users = action.payload.users;
 
-      console.log('ACCOUNT REDUCER', actionUsersType.Success);
+    case actionAccountType.Success:
+      console.log('ACCOUNT REDUCER', actionAccountType.Success);
 
-      const newState = {
+      return {
         ...state,
-        id: users.id,
-        name: users.name,
-        initialValue: users.initialValue,
-        transactions: [...state.transactions, users.transactions],
+        id: action.payload.id,
+        name: action.payload.name,
+        initialValue: action.payload.initialValue,
+        transactions: [...state.transactions, action.payload.transactions],
         loading: false,
         error: false,
       };
 
-      console.log(newState);
-      return newState;
-
-    case actionUsersType.Fail:
-      console.log('ACCOUNT REDUCER', actionUsersType.Fail);
+    case actionAccountType.Fail:
+      console.log('ACCOUNT REDUCER', actionAccountType.Fail);
 
       return {
         ...state,
         error: true,
       };
+
     default:
       return {...state};
   }
